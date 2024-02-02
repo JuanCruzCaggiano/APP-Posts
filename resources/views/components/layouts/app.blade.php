7b03773eea8081171c8f34c350b34a7993843ef1
JuanCruzCaggiano <juancruzcaggiano@hotmail.com>
2024-02-02 15:25:39 -0300

    Actualizacion

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Aprendible - {{ $title ?? ''}}</title>
+    <title>Laravel - {{ $title ?? ''}}</title>
     <meta name="description" content="{{ $metaDescription ?? 'Default meta description'}}">
     @vite(['resources/css/app.scss', 'resources/js/app.js'])
 </head>
