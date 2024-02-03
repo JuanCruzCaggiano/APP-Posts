932e4d6fb2d6c6ecbe1fabb37f61f2f4105553c3
JuanCruzCaggiano <juancruzcaggiano@hotmail.com>
2024-02-03 02:54:46 -0300

    Actualizo composer

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit14df23e9f3536b2a91b4502e0738cfc6::getLoader();
+return ComposerAutoloaderInit9c491b8531eec05ba41a11d9276a5749::getLoader();
